3f83054e8248cbb96198192c929340a0f1e07076
fedorenkood <fedorenkoalex23@gmail.com>
2021-11-30 22:44:23 -0500

    Fixed generated data

---
+++ drop_all_tables.sql
@@ -0,0 +1,9 @@
+
+drop table Responses;
+drop table Possible_Answers;
+drop table Permissions;
+drop table Questions;
+drop table Questionnaires;
+drop table Users;
+drop table Subscription;
+drop table Roles;
